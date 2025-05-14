2a3d4573c09d2ba2c4a531947554b39f8f9c0a0c
ssu <74248726+azuressu@users.noreply.github.com>
2025-05-14 22:46:08 +0900

    init commit

---
+++ src/test/java/com/meca/MecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
