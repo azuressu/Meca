3a150fd9fe851fba65b72f25f893c9568afe53ff
ssu <74248726+azuressu@users.noreply.github.com>
2025-05-16 17:37:47 +0900

    폴더 관련 로직 연결

--- src/main/java/com/meca/common/controller/PageController.java
+++ src/main/java/com/meca/common/controller/PageController.java
@@ -1,10 +1,7 @@
 package com.meca.common.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
@@ -12,16 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping("/")
-    public String mainPage(Model model, @AuthenticationPrincipal UserDetails userDetails) {
-        if (userDetails != null) {
-//            String username = userDetails.getUsername();
-//
-//            // 예를 들어, 사용자 폴더 리스트를 서비스에서 가져온다고 가정
-//            List<Folder> folders = folderService.getFoldersByUsername(username);
-//
-//            model.addAttribute("username", username);
-//            model.addAttribute("folders", folders);
-        }
+    public String mainPage() {
         return "folder";  // folder.html 뷰 리턴
     }
 
